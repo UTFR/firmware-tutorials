24cbf003eb371639ecebfd15398d6dd57a7f3962
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2025-10-08 23:29:40 -0400

    lec 3

--- src/mutex.cpp
+++ src/mutex.cpp
@@ -1,53 +1,42 @@
-#include <Arduino.h>
-#include <stdbool.h>
+#include "portable/portmacro.h"
+#include <arduino_freertos.h>
+#include <semphr.h>
+#include <queue.h>
+
+#define NUM_ITERS 1000
+#define CONTROL_LOOP_PERIOD_MS 100
 
 typedef struct {
   double torque;
   int dir;
 } torque_cmd_t;
 
-typedef int mutex_t;
-
-extern mutex_t mutex_create(void);
-
-// What would happen without volatile?
-extern volatile bool mutex_take(mutex_t mutex);
-extern volatile bool mutex_give(mutex_t mutex);
 extern void spin_motor(torque_cmd_t torque_command);
 
-#define NUM_ITERS 1000
-#define CONTROL_LOOP_PERIOD_MS 100
-
 static torque_cmd_t torque_cmd;
-static mutex_t torque_cmd_mutex;
+static SemaphoreHandle_t torque_cmd_mutex;
 
-void initialize_control_loop(void) { torque_cmd_mutex = mutex_create(); }
+void initialize_control_loop(void) {
+  torque_cmd_mutex = xSemaphoreCreateMutex();
+}
 
 void motor_control_thread(void) {
   for(;;) {
-    delay(CONTROL_LOOP_PERIOD_MS);
-
-    // Take ownership over torque command
-    while(!mutex_take(torque_cmd_mutex)) {}
-
+    xSemaphoreTake(torque_cmd_mutex, portMAX_DELAY);
     spin_motor(torque_cmd);
-
-    // Give up ownership over torque command
-    mutex_give(torque_cmd_mutex);
+    xSemaphoreGive(torque_cmd_mutex);
   }
 }
 
 void torque_calculator_thread(void) {
   for(;;) {
-    delay(CONTROL_LOOP_PERIOD_MS);
-
-    // Take ownership over torque command
-    while(!mutex_take(torque_cmd_mutex)) {}
+    xSemaphoreTake(torque_cmd_mutex, portMAX_DELAY);
 
     torque_cmd.torque = random(0, 2300) * 0.1;
     torque_cmd.dir = random(0, 1);
 
     // Give up ownership over torque command
-    mutex_give(torque_cmd_mutex);
+    xSemaphoreGive(torque_cmd_mutex);
   }
 }
+
--- src/queue.cpp
+++ src/queue.cpp
@@ -1,4 +1,7 @@
-#include <Arduino.h>
+#include "portable/portmacro.h"
+#include <arduino_freertos.h>
+#include <semphr.h>
+#include <queue.h>
 #include <stdbool.h>
 
 typedef struct {
@@ -6,39 +9,23 @@ typedef struct {
   int dir;
 } torque_cmd_t;
 
-typedef int mutex_t;
-typedef int queue_t;
-
-extern mutex_t mutex_create(void);
-extern volatile bool mutex_take(mutex_t mutex);
-extern volatile bool mutex_give(mutex_t mutex);
-
-extern queue_t queue_create(int num_elements, size_t element_size);
-extern volatile bool queue_send(queue_t queue, void *data);
-extern volatile bool queue_receive(queue_t queue, void *data);
-
 extern void spin_motor(torque_cmd_t torque_command);
 
 #define NUM_ITERS 1000
 #define CONTROL_LOOP_PERIOD_MS 100
 
-static queue_t torque_cmd_queue;
+static QueueHandle_t torque_cmd_queue;
 
 void initialize_control_loop(void) {
-  torque_cmd_queue = queue_create(128, sizeof(torque_cmd_t));
+  // torque_cmd_queue = queue_create(128, sizeof(torque_cmd_t));
+  torque_cmd_queue = xQueueCreate(128, sizeof(torque_cmd_t));
 }
-
-// I now have a question about the call to `delay`
-// Remind me if i forget to ask it
 
 void motor_control_thread(void) {
   torque_cmd_t cmd;
 
   for(;;) {
-    delay(CONTROL_LOOP_PERIOD_MS);
-
-    while(!queue_receive(torque_cmd_queue, &cmd)) {}
-
+    xQueueReceive(torque_cmd_queue, &cmd, portMAX_DELAY);
     spin_motor(cmd);
   }
 }
@@ -52,6 +39,6 @@ void torque_calculator_thread(void) {
     cmd.torque = random(0, 2300) * 0.1;
     cmd.dir = random(0, 1);
 
-    while(!queue_send(torque_cmd_queue, &cmd)) {}
+    xQueueSend(torque_cmd_queue, &cmd, 0);
   }
 }
