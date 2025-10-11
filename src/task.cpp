56311bc89dbf4ac934ef5c6cf8442b79c1b63ac7
Joseph <115049616+Joseph-Mi@users.noreply.github.com>
2025-10-10 23:24:36 -0400

    updated task functionality

--- src/task.cpp
+++ src/task.cpp
@@ -1,18 +1,20 @@
 #include <arduino_freertos.h>
 #include "foo_task.h"
 
-static void foo_task(void *p);
+static void task(void *p);
 
-#define FOO_STACK_DEPTH 1024
+#define FOO_STACK_DEPTH
 #define FOO_PRIORITY tskIDLE_PRIORITY + 1
 
 TaskHandle_t foo;
 
-void create_foo_task(void) {
+void create_task(void) {
   xTaskCreate(foo_task, "foo", FOO_STACK_DEPTH, NULL, FOO_PRIORITY, &foo);
   xTaskCreate(foo_task, "foo", FOO_STACK_DEPTH, NULL, FOO_PRIORITY, NULL);
 }
 
 static void foo_task(void *p) {
-  for(;;) {}
+  for(;;) {
+    /* make LEDs of FC blink */
+  }
 }
